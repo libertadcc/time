602a1593c6c0134ef3f5bb4bf508e108bc070456
libertadcc <libertadchapinalcervantes@gmail.com>
2019-09-09 20:15:44 +0200

    First steps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import moment from 'moment';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.hey = this.hey.bind(this)
+  }
+  componentDidMount() {
+    this.hey();
+  }
+  hey() {
+    let hey = moment().calendar(); 
+    console.log(hey)
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>¿Cuánto tiempo llevamos?</p>
+        </header>
+        <main>
+
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
